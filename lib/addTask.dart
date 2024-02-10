f49b933181caff895ced09a5c1b0933249346163
janushan12 <janu.1999.12@gmail.com>
2024-02-10 13:36:27 +0530

    Third changes

--- lib/addTask.dart
+++ lib/addTask.dart
@@ -1,5 +1,9 @@
+import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:task_management_application/main.dart';
+import 'package:http/http.dart' as http;
+import 'package:intl/intl.dart';
+
 
 class AddTask extends StatefulWidget {
   const AddTask({super.key});
@@ -13,6 +17,32 @@ class _AddTaskState extends State<AddTask> {
   TextEditingController nameController=TextEditingController();
   TextEditingController descriptionController=TextEditingController();
   DateTime selecteddate=DateTime.now();
+
+  Future<void> addTask() async {
+    const apiUrl = 'http://localhost:3000/tasks';
+
+    // Format the date before sending it to the server
+    final formattedDate = DateFormat('yyyy-MM-dd').format(selecteddate);
+
+    final response = await http.post(
+      Uri.parse(apiUrl),
+      headers: {'Content-Type':'application/json'},
+      body: jsonEncode({
+        'title':nameController.text,
+        'description':descriptionController.text,
+        'dueDate': formattedDate,
+      })
+    );
+
+    if(response.statusCode==201) {
+      // Task added successfully
+      Navigator.pop(context); // Go back
+    } else {
+      // Handle error
+      print('Error adding task: ${response.statusCode}');
+    }
+
+  } 
 
   Future<void> _selectdate(BuildContext context) async {
     final DateTime? picked = await showDatePicker(
@@ -34,7 +64,7 @@ class _AddTaskState extends State<AddTask> {
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Colors.blue,
-        title: Text(
+        title: const Text(
           "Add New Task",
           style: TextStyle(
             color: Colors.white,
@@ -43,7 +73,7 @@ class _AddTaskState extends State<AddTask> {
         ),
       ),
       body: Padding(
-        padding: EdgeInsets.all(10.0),
+        padding: const EdgeInsets.all(10.0),
         child: Form(
           key:_formKey,
           child: Column(
@@ -51,7 +81,7 @@ class _AddTaskState extends State<AddTask> {
             children: [
               TextFormField(
                 controller: nameController,
-                decoration: InputDecoration(labelText: "Task Name:"),
+                decoration: const InputDecoration(labelText: "Task Name:"),
                 validator: (value){
                   if(value==null || value.isEmpty) {
                     return "Please enter the task name";
@@ -61,7 +91,7 @@ class _AddTaskState extends State<AddTask> {
               ),
               TextFormField(
                 controller: descriptionController,
-                decoration: InputDecoration(labelText: "Description:"),
+                decoration: const InputDecoration(labelText: "Description:"),
                 validator: (value){
                   if(value==null || value.isEmpty) {
                     return "Please enter description";
@@ -73,14 +103,14 @@ class _AddTaskState extends State<AddTask> {
                 decoration: InputDecoration(
                   labelText: "Due Date:",
                   suffixIcon: IconButton(
-                    icon: Icon(Icons.calendar_today),
+                    icon: const Icon(Icons.calendar_today),
                     onPressed: ()=> _selectdate(context),
                   )
                 ),
                 readOnly: true,
                 onTap: ()=> _selectdate(context),
                 controller: TextEditingController(
-                  text: "${selecteddate.toLocal()}".split(' ')[0],
+                  text: DateFormat('yyyy-MM-dd').format(selecteddate),
                 ),
                 validator: (value) {
                   if(value==null || value.isEmpty) {
@@ -89,24 +119,22 @@ class _AddTaskState extends State<AddTask> {
                   return null;
                 },
               ),
-              SizedBox(height: 20,),
-              Container(
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceAround,
-                  children: [
-                    ElevatedButton(onPressed: (){
-                      Navigator.push(context, MaterialPageRoute(builder: (context)=>const MyHomePage(title: 'Task Management App',)),);
-                    }, child: Text("Task List"),),
-                    ElevatedButton(onPressed: (){
-                    // validate the form before submitting
-                    if(_formKey.currentState?.validate() ?? false) {
-                      // Form  is valid, perform your action
-                      Navigator.push(context, MaterialPageRoute(builder: (context)=>const MyHomePage(title: 'Task Management App',)),);
-                    }
-                    }, child: Text("Add"),
-                    ),
-                  ]
-                ),
+              const SizedBox(height: 20,),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  ElevatedButton(onPressed: (){
+                    Navigator.push(context, MaterialPageRoute(builder: (context)=>const MyHomePage(title: 'Task Management App',)),);
+                  }, child: const Text("Task List"),),
+                  ElevatedButton(onPressed: (){
+                  // validate the form before submitting
+                  if(_formKey.currentState?.validate() ?? false) {
+                    // Form  is valid, perform your action
+                    addTask();
+                  }
+                  }, child: const Text("Add"),
+                  ),
+                ]
               ),
             ],
           ),
